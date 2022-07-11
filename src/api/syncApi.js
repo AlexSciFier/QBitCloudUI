7b7787879a2db9bb0cb29cd1e347167e5b20ab60
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-07-11 17:44:04 +0300

    add sync api

--- src/api/syncApi.js
+++ src/api/syncApi.js
@@ -1,11 +1,16 @@
 import { get } from "./baseApi";
-const endpoint = "/api/v2/sync";
 
-export var getMainData = async () => {
-  let res = await get(endpoint + "/maindata");
-  if (res.ok) {
-    return await res.json();
-  } else {
-    return undefined;
-  }
-};
+export default class Sync {
+  static #endpoint = "/api/v2/sync";
+  static #rid = 0;
+  static getMainData = async (fullUpdate = false) => {
+    if (fullUpdate) this.#rid = 0;
+    let res = await get(this.#endpoint + "/maindata", { rid: this.#rid });
+    this.#rid = res?.rid || 0;
+    return res;
+  };
+
+  static getTorrentPeers = async (hash) => {
+    return await get(this.#endpoint + "/torrentPeers", { hash });
+  };
+}
