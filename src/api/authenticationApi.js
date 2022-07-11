359e6ce3f1d0d697d3b71c46e6d9c09c5c67f232
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-07-11 17:43:50 +0300

    add auth api

--- src/api/authenticationApi.js
+++ src/api/authenticationApi.js
@@ -1,18 +1,20 @@
 import { get } from "./baseApi";
-const endpoint = "/api/v2/auth";
 
-export var login = async (username, password) => {
-  let res = await get(endpoint + "/login", { username, password });
-  console.log(res.headers.get("set-cookie"));
-  if (res.ok) {
-    if ((await res.text()) === "Ok.") return { ok: true, message: "" };
-    return { ok: false, message: await res.text() };
-  } else {
-    return { ok: false, message: await res.text() };
-  }
-};
+export default class Authentication {
+  static #endpoint = "/api/v2/auth";
+  /**
+   *
+   * @param {string} username
+   * @param {string} password
+   * @returns
+   */
+  static login = async (username, password) => {
+    let res = await get(this.#endpoint + "/login", { username, password });
+    if (res === "Ok.") return { ok: true, message: "" };
+    return { ok: false, message: res };
+  };
 
-export var logout = async () => {
-  let res = await get(endpoint + "/logout");
-  return res.status === 200;
-};
+  static logout = async () => {
+    return await get(this.#endpoint + "/logout");
+  };
+}
