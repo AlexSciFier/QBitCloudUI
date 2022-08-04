a7eccac9165d7be70352afbcefa3351d09fe3baf
AlexSciFier <66871322+AlexSciFier@users.noreply.github.com>
2022-08-04 10:31:00 +0300

    custom color selector refactoring

--- src/layouts/Settings/Theme.jsx
+++ src/layouts/Settings/Theme.jsx
@@ -46,33 +46,42 @@ function ColorSwitcher() {
             ></div>
           </label>
         ))}
-
-        <label className="relative">
-          <input
-            type={"color"}
-            value={mainColor}
-            className="hidden"
-            onChange={(e) => {
-              setCustomColorSelected(true);
-              if (customColorSelected) {
-                setMainColor(e.target.value);
-              }
-            }}
-          ></input>
-          <input
-            type={"radio"}
-            name="main-color"
-            className="hidden peer"
-            checked={customColorSelected}
-          ></input>
-          <div
-            className="w-12 h-12 rounded-full peer-checked:ring dark:ring-white ring-black"
-            style={{ backgroundColor: mainColor }}
-          ></div>
-          <PencilIcon className="w-6 h-6 absolute top-3 left-3 text-white" />
-        </label>
+        <CustomColorSelector
+          customColorSelected={customColorSelected}
+          setCustomColorSelected={setCustomColorSelected}
+        />
       </div>
     </div>
+  );
+}
+
+function CustomColorSelector({ setCustomColorSelected, customColorSelected }) {
+  const { mainColor, setMainColor } = useTheme();
+  return (
+    <label className="relative">
+      <input
+        type={"color"}
+        value={mainColor}
+        className="hidden"
+        onChange={(e) => {
+          setCustomColorSelected(true);
+          if (customColorSelected) {
+            setMainColor(e.target.value);
+          }
+        }}
+      ></input>
+      <input
+        type={"radio"}
+        name="main-color"
+        className="hidden peer"
+        checked={customColorSelected}
+      ></input>
+      <div
+        className="w-12 h-12 rounded-full peer-checked:ring dark:ring-white ring-black"
+        style={{ backgroundColor: mainColor }}
+      ></div>
+      <PencilIcon className="w-6 h-6 absolute top-3 left-3 text-white" />
+    </label>
   );
 }
 
